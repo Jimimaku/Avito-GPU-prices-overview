3136387207c57e994abe7254ef1249da08a2cc67
sesap <nooooooooob@yandex.ru>
2018-04-26 04:09:21 +0300

    update

--- Avito_Cleaning.py
+++ Avito_Cleaning.py
@@ -1,51 +1,78 @@
 from bs4 import BeautifulSoup
-import time
 from datetime import datetime, timedelta
-meterDayBfYstr = 0
-meterYstr = 0
+import time, sched
+import numpy as np
+import csv
 
-DBfYesterday = datetime.now() - timedelta(days=2)
-DBfYesterday = DBfYesterday.strftime('%m_%d_%Y')
-YesterdayDate = datetime.now() - timedelta(days=1)
-YesterdayDate = YesterdayDate.strftime('%m_%d_%Y')
-Yesterday = 'Вчера'
+s = sched.scheduler(time.time, time.sleep)
 
-path = "C:/WorkData/Avito/Avito_Daily/GPU_04_24_2018.html"
+def Avito_Cleaning():
 
-with open(path, 'r', encoding='utf-8') as f:
-    data = f.read()
+    DBfYesterday = datetime.now() - timedelta(days=2)
+    DBfYesterday = DBfYesterday.strftime('%m_%d_%Y')
+    YesterdayDate = datetime.now() - timedelta(days=1)
+    YesterdayDate = YesterdayDate.strftime('%m_%d_%Y')
+    TodayDate = datetime.now()
+    TodayDate = TodayDate.strftime('%m_%d_%Y')
 
-soup = BeautifulSoup(data, 'html.parser')
-print(soup.prettify())
-print(type(soup))
-print('__________________________')
+    path = "C:/WorkData/Avito/Avito_Daily/GPU_" + YesterdayDate #!!!
 
-namelist = []
-reflist = []
-pricelist = []
-timelist = []
+    with open(path+".html", 'r', encoding='utf-8') as f:
+        data = f.read()
 
-for name in soup.find_all('a', class_="description-title-link", href=True):
-    namelist.append(name.text)
-    reflist.append(name['href'])
+    soup = BeautifulSoup(data, 'html.parser')
 
-print(len(namelist))# amount of ads
-print(len(reflist))
+    namelist = [] # columns in csv
+    reflist = []
+    pricelist = []
+    timelist = []
 
-for price in soup.find_all('div', class_="price"):
-    pricelist.append(price.text)
-print(len(pricelist))
+    for name in soup.find_all('a', class_="description-title-link", href=True):
+        namelist.append(name.text)
+        reflist.append(name['href'])
 
-DayAmountDayBeforeYesterday = DBfYesterday[3:5]
+    for price in soup.find_all('div', class_="price"):
+        pricelist.append(price.text)
 
-for date in soup.find_all('span', class_="date"):
-    timelist.append(date.text)
-    if DayAmountDayBeforeYesterday in date.text:
-        meterDayBfYstr+=1
-    if Yesterday in date.text[3:9]:
-        meterYstr+=1
-    print(date.text)
+    for date in soup.find_all('span', class_="date"):
+        if DBfYesterday[3:5] in date.text:
+            timelist.append(date.text)
+        elif 'Вчера' in date.text[3:9]:
+            timelist.append(YesterdayDate)
+        elif 'Сегодня' in date.text[3:11]:
+            timelist.append(TodayDate)
+        else:
+            timelist.append(date.text)
 
-print('Date = ', DBfYesterday, 'DayBefore Yesterday amount: ', meterDayBfYstr)
-print('Date = ', YesterdayDate, 'Yesterday amount: ', meterYstr)
-print(len(timelist))
+    YesterdayIndices = [i for i, x in enumerate(timelist) if x == YesterdayDate]
+    DataColumnsLen = len(namelist),len(pricelist),len(timelist),len(reflist)
+
+    namelist = np.array(namelist) #in order to pick only yesterday date
+    namelist = namelist[YesterdayIndices]
+    pricelist = np.array(pricelist)
+    pricelist = pricelist[YesterdayIndices]
+    timelist = np.array(timelist)
+    timelist = timelist[YesterdayIndices]
+    reflist = np.array(reflist)
+    reflist = reflist[YesterdayIndices]
+
+    output = zip(namelist,pricelist,timelist,reflist)
+
+    with open(path + ".csv", "w") as f:
+        writer = csv.writer(f)
+        for row in output:
+            writer.writerow(row)
+
+    print('Should be equal, total used ads', DataColumnsLen)
+    print('Total GPU ads per day ' + YesterdayDate, len(namelist))
+    print('Downloaded rows/Added rows (should be > 1)', DataColumnsLen[0]/len(namelist))
+    print('Saved to', path + ".csv")
+    #s.enter(60, 1, Avito_Cleaning)
+
+
+    return('Finished')
+
+Avito_Cleaning()
+
+#s.enter(60, 1, Avito_Cleaning)
+#s.run()
