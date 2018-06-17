a0faba4f5fc104f0c858f176f1483238f3cbaff8
sesap <nooooooooob@yandex.ru>
2018-06-17 20:42:40 +0300

    update

--- Avito_HTML_Download.py
+++ Avito_HTML_Download.py
@@ -13,10 +13,12 @@ for n in range(1,6): # 5
     g = Grab()
     print('starting page number ', n)
     time.sleep(random.uniform(30, 120))#random delay
-    avitourl = 'https://www.avito.ru/sankt-peterburg/tovary_dlya_kompyutera/komplektuyuschie/videokarty?p='+str(n)+'&view=list&s=104'
+    #avitourl = 'https://www.avito.ru/sankt-peterburg/tovary_dlya_kompyutera/komplektuyuschie/videokarty?p='+str(n)+'&view=list&s=104'
+    avitourl = 'https://www.avito.ru/sankt-peterburg/tovary_dlya_kompyutera/komplektuyuschie/videokarty?p='+str(n)+'&s=104&view=list'
     resp = g.go(avitourl)
     mystr = resp.unicode_body()
     print(type(mystr))
+    #print(mystr)
     #print('mystr', type(mystr), mystr)
     print('amount of вчера ', mystr.count('вчера'), 'meter ', meter)
     if mystr.count('вчера') < 4 and meter!=0:
